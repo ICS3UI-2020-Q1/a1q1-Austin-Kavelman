50bc10238d58b980ccb61ee9736d7ae81241e12d
AustinKavelman <kavea9410@wrdsb.ca>
2020-09-14 18:01:19 +0000

    Finished convert a temperature from Celsius to Fahrenheit

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *To convert a temperature from Celsius to Fahrenheit
+ * @author Austin Kavelman
  */
 public class Main {
 
@@ -9,7 +11,20 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create a scanner to use input
+    Scanner input = new Scanner(System.in);
+
+    //ask the user for an amount of degrees celsius
+    System.out.println("Enter a temperature to convert in degrees celsius");
+
+    // Allow the user to enter degrees celsius
+    int celsius = input.nextInt();
+
+    //Calculates the user's temperature from celsius to fahrenheit
+    int fahrenheit = (celsius*9)/5+32;
+    
+    //Displays the farhenhiet conversion
+    System.out.println(celsius + " in celsius is the same as " + fahrenheit + " in fahrenheit");
     
   }
 }
